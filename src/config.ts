21c4bad4dd8fe530615544a7f4a88d17b922cf64
kinxa0 <kinzangdorji@dhi.bt>
2025-05-20 17:08:54 +0600

    Update package name and RPC URL

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const networkConfig = {
   testnet: {
     // URL: 'https://rpc-amoy.polygon.technology',
     // CONTRACT_ADDRESS: '0xcB80F37eDD2bE3570c6C9D5B0888614E04E1e49E',
-    URL: 'https://ethereum-sepolia.blockpi.network/v1/rpc/public',
+    URL: 'https://eth-sepolia.g.alchemy.com/v2/m0SEA2hYFe149nEdKYMPao8Uv_ZrPqeM',
     CONTRACT_ADDRESS: '0x7423254347AbE61F7606d0b850CAa1233Fb660bC',
   },
   mainnet: {
